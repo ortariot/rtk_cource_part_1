8d564a1c7f487135612188718dec6cfe6cf97eb1
ortariot <v.n.homutov@gmail.com>
2023-04-26 16:23:34 +0700

    fix MR notifications

--- data_store_tools.py
+++ data_store_tools.py
@@ -1,178 +1,254 @@
 from random import randint
-from pprint import pprint
+from typing import Optional
 
 from sqlalchemy import create_engine, select, update, insert
-from sqlalchemy.orm import Session, Load
-
+from sqlalchemy.orm import Session, Load, sessionmaker
+from sqlalchemy.exc import NoResultFound
 import bcrypt
 
 from config import SQLALCHEMY_DATABASE_URI
-from models import Users, Plans, Tabs, Services, Accommodation
+from models import Users, Plans, Tabs, Services, Accommodations
 
 
-def crate_user(engine, name, phone, mail, login, password):
-    """
-    1.Написать функцию, которая принимает
-    данные пользователя и создает его.
-    """
-    with Session(engine) as session:
-        user = Users(name=name,
-                     phone=phone,
-                     mail=mail,
-                     login=login,
-                     password=bcrypt.hashpw(
-                         password.encode(), bcrypt.gensalt()),
-                     )
+class DataStoreTools():
+
+    def __init__(self, uri: str) -> None:
+        self.engine = create_engine(uri)
+
+    def session_executor(proc):
+        def wrapper(*args, **kwargs):
+            self = args[0]
+            engine = self.engine
+            sesion_factory = sessionmaker(
+                engine,
+                expire_on_commit=False
+            )
+            with sesion_factory() as session:
+                with session.begin():
+                    try:
+                        res = proc(*args, **kwargs, session=session)
+                    except NoResultFound as e:
+                        print(f'error  - {e}')
+                        return None
+            return res
+        return wrapper
+
+    @session_executor
+    def crate_user(
+        self, name: str, phone: str, mail: str,
+        login: str, password: str, session: Session
+    ) -> None:
+        """
+        1.Написать функцию, которая принимает
+        данные пользователя и создает его
+        """
+        user = Users(
+            name=name,
+            phone=phone,
+            mail=mail,
+            login=login,
+            password=bcrypt.hashpw(
+                password.encode(), bcrypt.gensalt()),
+        )
         session.add(user)
-        session.commit()
 
-
-def crate_plan(engine, name, desc, price, service_id):
-    with Session(engine) as session:
-        plan = Plans(name=name,
-                     service_id=service_id,
-                     desc=desc,
-                     price=price,
-                     )
+    @session_executor
+    def create_plan(
+        self, name: str, desc: str, price: int,
+        service_id: int, session: Session
+    ) -> None:
+        plan = Plans(
+            name=name,
+            service_id=service_id,
+            desc=desc,
+            price=price
+        )
         session.add(plan)
-        session.commit()
 
-
-def create_service(engine, name):
-    with Session(engine) as session:
+    @session_executor
+    def create_service(self, name: str, session: Session) -> None:
         service = Services(name=name)
         session.add(service)
-        session.commit()
 
-
-def create_tab(engine, name, user_id):
-    '''
-    2.Написать функцию, которая принимает название ЛС,
-    создает его и привязывает к определенному пользователю.
-    Номер счета генерируется автоматически.
-    '''
-    with Session(engine) as session:
-        tab = Tabs(number=randint(1000000, 9999999),
-                   name=name,
-                   user_id=user_id,
-                   )
+    @session_executor
+    def create_tab(self, name: str, user_id: int, session: Session) -> None:
+        '''
+        2.Написать функцию, которая принимает название ЛС,
+        создает его и привязывает к определенному пользователю.
+        Номер счета генерируется автоматически
+        '''
+        tab = Tabs(
+            number=randint(1000000, 9999999),
+            name=name,
+            user_id=user_id,
+        )
         session.add(tab)
-        session.commit()
 
+    @session_executor
+    def create_accommodation(
+        self, service_name: str, plan_name: str,
+        tab_id: int, addres: str, session: Session
+    ) -> None:
+        """
+        3.	Написать функцию, которая:
+            Получает услугу из перечня по ее английскому наименованию (коду).
+            Получает тариф услуги из перечня по его английскому
+            наименованию (коду). Привязывает услугу с выбранным тарифом к
+            определенному ЛС в статусе Активна
+        """
 
-def create_accommodation(engine, service_name, plan_name, tab_id, addres):
-    """
-    3.	Написать функцию, которая:
-        Получает услугу из перечня по ее английскому наименованию (коду).
-        Получает тариф услуги из перечня по его английскому
-        наименованию (коду). Привязывает услугу с выбранным тарифом к
-        определенному ЛС в статусе Активна.
-    """
-    with Session(engine) as session:
-        query_service = select(Services).where(Services.name == service_name)
-        query_service_result = session.execute(query_service)
-        service = query_service_result.scalar_one()
+        service = self.get_service(service_name=service_name)
+        plan = self.get_plan(service_id=service.id, plan_name=plan_name)
+        tab = self.get_tab(tab_id=tab_id)
 
-        query_plan = select(Plans).options(Load(Plans)).where(Plans.name == plan_name,
-                                                              Plans.service == service
-                                                              )
-        query_plan_result = session.execute(query_plan)
-        plan = query_plan_result.scalar_one()
-
-        query_tab = select(Tabs).options(Load(Tabs)).where(Tabs.id == tab_id)
-        query_tab_result = session.execute(query_tab)
-        tab = query_tab_result.scalar()
-
-        ac_query = insert(Accommodation).values(service_id=service.id,
-                                                addres=addres,
-                                                tab_id=tab.id,
-                                                plan_id=plan.id,
-                                                )
+        try:
+            ac_query = insert(Accommodations).values(
+                service_id=service.id,
+                addres=addres,
+                tab_id=tab.id,
+                plan_id=plan.id,
+            )
+        except AttributeError as e:
+            print(f'error  - {e}')
+            return None
 
         session.execute(ac_query)
-        session.commit()
 
-
-def update_plan(engine, tab_id, plan_id):
-    '''
-    4.	Написать функцию, которая позволяет изменить тариф услуги по ID ЛС.
-    '''
-    with Session(engine) as session:
+    @session_executor
+    def update_plan(
+        self, tab_id: int, plan_id: int, session: Session
+    ) -> None:
+        '''
+        4. Написать функцию, которая позволяет изменить тариф услуги по ID ЛС
+        '''
         query_plan = select(Plans).options(
             Load(Plans)).where(Plans.id == plan_id)
         query_plan_result = session.execute(query_plan)
         plan = query_plan_result.scalar_one()
 
         if plan:
-            query = update(Accommodation).where(
-                Accommodation.tab_id == tab_id).values(plan_id=plan.id)
+            query = update(Accommodations).where(
+                Accommodations.tab_id == tab_id).values(plan_id=plan.id)
 
             session.execute(query)
-        session.commit()
+            session.commit()
 
-
-def block_service(engine, tab_id):
-    """
-    5.	Написать функцию, которая позволяет заблокировать услугу по ID ЛС.
-    """
-    with Session(engine) as session:
-        query = update(Accommodation).where(
-            Accommodation.tab_id == tab_id).values(status=False)
+    @session_executor
+    def block_service(self, tab_id: int, session: Session) -> None:
+        """
+        5.	Написать функцию, которая позволяет заблокировать услугу по ID ЛС
+        """
+        query = update(Accommodations).where(
+            Accommodations.tab_id == tab_id).values(status=False)
         session.execute(query)
-        session.commit()
 
-
-def about_user(engine, user_id):
-    """
-    6.	Написать функцию, которая по ID пользователя возвращает
-    список всех активных ЛС, включая подключенные услуги и тарифы.  
-    """
-    with Session(engine) as session:
+    @session_executor
+    def get_tab(
+        self, user_id: Optional[int] = None,
+        tab_id: Optional[int] = None, one: bool = True,
+        session: Optional[Session] = None
+    ) -> Tabs:
 
         query_tab = select(Tabs).options(
-            Load(Tabs)).where(Tabs.user_id == user_id)
+            Load(Tabs)).where(
+            Tabs.user_id == user_id if user_id else Tabs.id == tab_id
+        )
         query_tab_result = session.execute(query_tab)
-        tabs = query_tab_result.scalars().all()
+        if one:
+            tabs = query_tab_result.scalar_one()
+        else:
+            tabs = query_tab_result.scalars().all()
 
-        res = {'user_id': user_id,
+        return tabs
+
+    @session_executor
+    def get_accommodation(self, tab: Tabs, session: Session):
+
+        query_accommodation = select(Accommodations).options(
+            Load(Accommodations)
+        ).where(Accommodations.tab == tab)
+        query_accommodation_res = session.execute(query_accommodation)
+        accommodation = query_accommodation_res.scalars().first()
+
+        return accommodation
+
+    @session_executor
+    def get_service(
+        self, service_id: Optional[int] = None,
+        service_name: Optional[str] = None,
+        session: Session = None
+    ) -> Services:
+
+        query_service = select(Services).where(
+            Services.id == service_id if service_id
+            else Services.name == service_name
+        )
+        query_service_result = session.execute(query_service)
+        service = query_service_result.scalar_one()
+
+        return service
+
+    @session_executor
+    def get_plan(
+        self, service_id: int,
+        plan_id: Optional[int] = None,
+        plan_name: Optional[str] = None,
+        session: Optional[Session] = None
+    ) -> Plans:
+
+        query_plan = select(Plans).options(
+            Load(Plans)).where(
+                Plans.id == plan_id if plan_id
+            else Plans.name == plan_name,
+                Plans.service_id == service_id
+        )
+
+        query_plan_result = session.execute(query_plan)
+        plan = query_plan_result.scalar_one()
+        return plan
+
+    def about_user(self, user_id: int) -> dict:
+        """
+        6.	Написать функцию, которая по ID пользователя возвращает
+        список всех активных ЛС, включая подключенные услуги и тарифы.
+        """
+
+        tabs = self.get_tab(user_id, one=False)
+        tabs = tabs if tabs else []
+
+        res = {'user_id': user_id if tabs else "unknown_user",
                'tabs': []}
 
         for tab in tabs:
-            res['tabs'].append({'number': tab.number,
-                                'name': tab.name,
-                                'create date': tab.create_date,
-                                'update date': tab.update_date,
-                                }
-                               )
+            res['tabs'].append(
+                {'number': tab.number,
+                    'name': tab.name,
+                    'create_date': tab.create_date,
+                    'update_date': tab.update_date,
+                 }
+            )
 
-            query_accommodation = select(Accommodation).options(
-                Load(Accommodation)
-            ).where(Accommodation.tab == tab)
-            query_accommodation_res = session.execute(query_accommodation)
-            accommodation = query_accommodation_res.scalars().first()
-            print(accommodation)
+            accommodation = self.get_accommodation(tab)
+
             if accommodation:
-                query_service = select(Services).where(
-                    Services.id == accommodation.service_id)
-                query_service_result = session.execute(query_service)
-                service = query_service_result.scalar_one()
 
-                query_plan = select(Plans).options(
-                    Load(Plans)).where(Plans.id == accommodation.plan_id, Plans.service_id == accommodation.service_id)
-                query_plan_result = session.execute(query_plan)
-                plan = query_plan_result.scalar_one()
+                service = self.get_service(accommodation.service_id)
+                plan = self.get_plan(
+                    accommodation.service_id,
+                    accommodation.plan_id
+                )
 
-                res['tabs'][-1].update({'service': {'name': service.name,
-                                                    'plan': plan.name,
-                                                    'price': plan.price,
-                                                    'description': plan.desc
-                                                    }
-                                        }
-                                       )
+                try:
+                    res['tabs'][-1].update(
+                        {'service': {'name': service.name,
+                                     'plan': plan.name,
+                                     'price': plan.price,
+                                     'description': plan.desc
+                                     }
+                         }
+                    )
+                except AttributeError as e:
+                    print(f'error  - {e}')
+                    return None
 
         return res
-
-
-if __name__ == '__main__':
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
-    print(about_user(engine, 1))
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
-from sqlalchemy import Column, ForeignKey, Integer, LargeBinary, Boolean, String, DateTime, BigInteger, create_engine
+from sqlalchemy import (
+    Column, ForeignKey, Integer, LargeBinary, Boolean,
+    String, DateTime, BigInteger, create_engine
+)
 from sqlalchemy.orm import declarative_base, declarative_mixin, relationship
 
 from config import SQLALCHEMY_DATABASE_URI
@@ -10,59 +13,67 @@ Base = declarative_base()
 
 @declarative_mixin
 class BaseModelMixin:
-    id = Column(BigInteger, primary_key=True)
-    create_date = Column(DateTime, nullable=False, default=datetime.utcnow)
-    update_date = Column(DateTime, nullable=False,
-                         default=datetime.utcnow, onupdate=datetime.utcnow)
+    id: int = Column(BigInteger, primary_key=True)
+    create_date: datetime = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+    update_date: datetime = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
 
 
 class Users(Base, BaseModelMixin):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
-    name = Column(String, nullable=False)
-    phone = Column(String, unique=True, nullable=False)
-    mail = Column(String, unique=True, nullable=False)
-    login = Column(String, unique=True, nullable=False)
-    password = Column(LargeBinary, nullable=False)
+    name: str = Column(String, nullable=False)
+    phone: str = Column(String, unique=True, nullable=False)
+    mail: str = Column(String, unique=True, nullable=False)
+    login: str = Column(String, unique=True, nullable=False)
+    password: LargeBinary = Column(LargeBinary, nullable=False)
 
 
 class Tabs(Base, BaseModelMixin):
-    __tablename__ = "tab"
+    __tablename__ = "tabs"
 
-    number = Column(Integer, nullable=False)
-    name = Column(String)
-    balance = Column(Integer, default=0)
-    user_id = Column(ForeignKey('user.id'), nullable=False)
-    user = relationship('Users')
-
-
-class Plans(Base, BaseModelMixin):
-    __tablename__ = 'plan'
-
-    service_id = Column(ForeignKey('service.id'), nullable=False)
-    service = relationship('Services')
-    name = Column(String, nullable=False)
-    desc = Column(String, nullable=False)
-    price = Column(Integer, nullable=False)
+    number: int = Column(Integer, nullable=False)
+    name: str = Column(String)
+    balance: int = Column(Integer, default=0)
+    user_id: int = Column(ForeignKey('users.id'), nullable=False)
+    user: Users = relationship('Users')
 
 
 class Services(Base, BaseModelMixin):
-    __tablename__ = 'service'
+    __tablename__ = 'services'
 
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
 
 
-class Accommodation(Base, BaseModelMixin):
-    __tablename__ = 'accommodation'
+class Plans(Base, BaseModelMixin):
+    __tablename__ = 'plans'
 
-    service_id = Column(ForeignKey('service.id'), nullable=False)
-    service = relationship('Services')
-    status = Column(Boolean, default=True)
-    addres = Column(String, nullable=False)
-    tab_id = Column(ForeignKey('tab.id'), nullable=False, unique=True)
-    tab = relationship('Tabs')
-    plan_id = Column(ForeignKey('plan.id'), nullable=False)
-    plan = relationship('Plans')
+    service_id: int = Column(ForeignKey('services.id'), nullable=False)
+    service: Services = relationship('Services')
+    name: str = Column(String, nullable=False)
+    desc: str = Column(String, nullable=False)
+    price: int = Column(Integer, nullable=False)
+
+
+class Accommodations(Base, BaseModelMixin):
+    __tablename__ = 'accommodations'
+
+    service_id: int = Column(ForeignKey('services.id'), nullable=False)
+    service: Services = relationship('Services')
+    status: bool = Column(Boolean, default=True)
+    addres: str = Column(String, nullable=False)
+    tab_id: int = Column(ForeignKey('tabs.id'), nullable=False, unique=True)
+    tab: Tabs = relationship('Tabs')
+    plan_id: int = Column(ForeignKey('plans.id'), nullable=False)
+    plan: Plans = relationship('Plans')
 
 
 if __name__ == '__main__':
